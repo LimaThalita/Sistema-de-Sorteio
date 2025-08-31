c34418433cae37e2902d542677c66ae72f947297
LimaThalita <vthalita56@gmail.com>
2025-08-31 15:12:58 -0300

    adição de rufar tambores

--- script.js
+++ script.js
@@ -14,24 +14,36 @@ function sortear(){
     let numero = escolherNumeroAleatorio(50);
     let posicaoCartela = escolherNumeroAleatorio(32);
     let cartela = selecionaCartelaPelaPosicao(posicaoCartela);
-    
+        
     let resultado = `${cartela.toUpperCase()} ${numero}`;
-    
-    document.getElementById('resultado').innerText = resultado;
 
-    Swal.fire({
-        title: resultado,   
-        icon: "success",
-        showConfirmButton: false,
-        timer: 1700,
-        didOpen: () => {
-            confetti({
-                particleCount: 200,
-                spread: 100,
-                origin: { y: 0.6 }
-            });
-        }
-    });
+    setTimeout(() => {
+        document.getElementById('resultado').innerHTML = '<img src="image/drumroll.gif" alt="Tambor">';
+        document.getElementById('resultado').classList.add("sorteando");
+
+        let audio = new Audio("audio/Rufem os Tambores.mp3"); // troque pelo nome do seu arquivo
+        audio.play();
+
+    }, 1200);
+
+    setTimeout(() => {
+        document.getElementById('resultado').classList.remove("sorteando");
+        document.getElementById('resultado').innerText = resultado;
+
+        Swal.fire({
+            title: resultado,   
+            icon: "success",
+            showConfirmButton: false,
+            timer: 1700,
+            didOpen: () => {
+                confetti({
+                    particleCount: 200,
+                    spread: 100,
+                    origin: { y: 0.6 }
+                });
+            }
+        });
+    },1200 + 2000);
 
 }
 
